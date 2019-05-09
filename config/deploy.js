171184f9ee917afc8a80239a127e9bae649c7734
Sebastian Gug <seb.gug@thameslabs.com>
2019-05-09 16:46:07 +0100

    initial commit

---
+++ config/deploy.js
@@ -0,0 +1,15 @@
+var path = require('path');
+var ghpages = require('gh-pages');
+
+var dir = path.resolve(path.join(__dirname, '../', 'dist'));
+
+ghpages.publish(dir, {
+  user: {
+    name: 'Swimlane',
+    email: 'developer@swimlane.com'
+  },
+  message: '(deploy): CI',
+  logger: function(message) {
+    console.log('gh-pages: ', message);
+  }
+});
